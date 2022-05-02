74e02cb7ac62769e6e9ebcfcf3cc9617ef2624d6
NCE20 <nce0130@naver.com>
2022-05-02 17:01:35 +0900

    Upload calculator

--- src/js/calculator/Calculator.js
+++ src/js/calculator/Calculator.js
@@ -2,8 +2,15 @@ class Calculator {
   plus = (a, b) => {
     return a + b;
   };
+  minus = (a, b) => {
+    return a - b;
+  };
+  multiple = (a, b) => {
+    return a * b;
+  };
+  divide = (a, b) => {
+    return a / b;
+  };
 }
-
-
 
 export default Calculator;
